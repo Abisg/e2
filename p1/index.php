8f51767c0e1e5206a9f96f17dc67f934e0fa35d5
Abigail Simpson-Givens <abs009@g.harvard.edu>
2023-09-26 03:48:45 +0000

    Establish project 1 SOC

--- p1/index.php
+++ p1/index.php
@@ -1,26 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project 1</title>
-</head>
 
-<body>
 
-    <h1>Project 1</h1>
-
-    <h2>Game Mechanics</h2>
-    <ul>
-        <li>...</li>
-    </ul>
-
-    <h2>Results</h2>
-    <ul>
-        <li>...</li>
-    </ul>
-
-</body>
-
-</html>
+require 'index-view.php';
